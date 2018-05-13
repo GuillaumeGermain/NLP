ca2631489d1c2e9b0e60c1cd243aa52a59dec9e1
Guillaume Germain <g.germain@me.com>
2018-05-13 18:02:38 +0200

    Improvements
    in nlp_util:
    Remove verboseSVM from ml_loop
    new build_sets
    in nlp_2:
    loop on vocab sizes and train size
    cleanup

--- nlp_2.py
+++ nlp_2.py
@@ -1,38 +1,11 @@
 """
 Natural Language Processing
-Version 2 with MORE data
+Bag or words model
+
+looping over vocabulary size, training size
+using classical classifier algorithms
 
 
-CONCLUSION
-
-After quite some exercise (very good practice with Python and Pandas, data processing)
-the new dataset used for training is too far from the test dataset.
-It could have been expected that more data would help, but it was not the case
-
-Surprisingly, the initial test taking into account the new dataset for testing, had very good results
-It could be interesting to actually use these ML algos on this new dataset...
-
-second, limiting the corpus to 4000 words may have noticeably deteriorated the performance
-It is very curious here that a simple logistic regression had the shortest fitting time and the best performance
-
-This has been confirmed by checking the distribution of the data
- datasets have a significantly different distribution
- the second dataset has a noticeably higher mean (0.67 vs 0.5)
- specially the standard deviation is VERY different (0.3 vs 0.5)
- this could explain why it is not useful to increase the performance
-
-
-one test: svm linear and logistic regression have consistently the best results
-naive bayes and svm sigmoid also were good on the original dataset
-let's experiment with only these ones
-
-
-Random Forest has consistently bad results and are pretty long to run
-SVM linear here seems the most interesting: longest run time, but best performance
-Naive Bayes which was best in the first dataset, has here only around 0.5 accuracy
-
-!! naive Bayes is not able to manage big amounts -> partial_fit for "online fitting" or further fitting on new datase chunks
-?? same for other methods?
 """
 
 
@@ -40,6 +13,8 @@ Naive Bayes which was best in the first dataset, has here only around 0.5 accura
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from scipy import sparse
+from sklearn.feature_extraction.text import CountVectorizer
 
 from nlp_util import *
 
@@ -47,18 +22,8 @@ from nlp_util import *
 dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting=3)
 dataset2 = pd.read_csv('newdata/1-restaurant-train.tsv', delimiter='\t', quoting=3)
 
-# Activate this to compare datasets
-# set the same scoring scale for both
-#dataset2.loc[dataset2['Liked'] == 1, 'Liked'] = 0.
-#dataset2.loc[dataset2['Liked'] == 2, 'Liked'] = 0.25
-#dataset2.loc[dataset2['Liked'] == 3, 'Liked'] = 0.5
-#dataset2.loc[dataset2['Liked'] == 4, 'Liked'] = 0.75
-#dataset2.loc[dataset2['Liked'] == 5, 'Liked'] = 1.
-#
 #dataset.describe()
 #dataset2.describe()
-#
-
 
 
 ## The second dataset has different values, from 1 to 5
@@ -69,89 +34,52 @@ dataset2.loc[dataset2['Liked'] < 3, 'Liked'] = 0
 dataset2.loc[dataset2['Liked'] > 3, 'Liked'] = 1
 
 
-## Compression
-#import seaborn as sns
-#
-#sparse_dataset = dataset
-##dense_size = np.array(dataset).nbytes/1e6
-##sparse_size = (sparse_dataset.data.nbytes + sparse_dataset.indptr.nbytes + sparse_dataset.indices.nbytes)/1e6
-#
-##sum(dataset.memory_usage())/ 1e6
-#
-#sns.barplot(['DENSE', 'SPARSE'], [dense_size, sparse_size])
-#plt.ylabel('MB')
-#plt.title('Compression')
 
 
 
+### LOOP OVER THIS
+#train_sizes = [1000, 2000, 5000, 10000, 15000, 20000, 30000, len(dataset2)]
+#vocab_sizes = [500, 800, 1000, 1500, 1800, 2000, 2200]
 
-# Append a part of the second biggest dataset to the main dataset
+# Pre-cabled loops over many configurations to use directly
+# In this case, only logistic regression (very short time, enables many tries in a short time)
+train_sizes = [20000, 25000, 30000]
+vocab_sizes = [2500, 3000, 3500,4000]
 TEST_SIZE = len(dataset)
-TRAIN_SIZE = len(dataset2) #10000
-VOCAB_SIZE = 2000
-
-
-dataset2 = dataset2[:TRAIN_SIZE]
-dataset = dataset.append(dataset2, ignore_index=True)
-
-
-corpus = build_corpus(dataset['Review'])
-
-# Creating the Bag of Words model
-from sklearn.feature_extraction.text import CountVectorizer
-cv = CountVectorizer(max_features=VOCAB_SIZE)
-X = cv.fit_transform(corpus).toarray() # creates a sparse matrix
-y = dataset.iloc[:, 1].values
-
-## Is X a sparse matrix? Well we already know that
-#from scipy.sparse import csr_matrix, isspmatrix
-#isspmatrix(csr_matrix([[5]]))
-
-
-
-# Commented as we do it differently here
-# the first 1000 lines of the dataset and hence X this is the test set
-# the remaining of X is the training set.
-# They are grouped together to set up the common vocabulary, cleaning and tokenization
-
-## Splitting the dataset into the Training set and Test set
-#from sklearn.model_selection import train_test_split
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
-
-
-# Handle sparsity
-from scipy import sparse
-X = sparse.csr_matrix(X)
-
-
-
-# Split X and Y to get back values of dataset and dataset2
-# dataset will be used for testing, dataset2 for training
-# X and y contain first dataset values, then dataset2 values
-X_train = X[TEST_SIZE:TEST_SIZE + TRAIN_SIZE]
-X_test = X[:TEST_SIZE]
-y_train = y[TEST_SIZE:TEST_SIZE + TRAIN_SIZE]
-y_test = y[:TEST_SIZE]
-
-# manually trim if needed
-#X_train = X_train[:500]
-#y_train = y_train[:500]
-
-
-
-
-
-# Loop over methods
+methods = ["logistic_regression"]
+scales = [False, True]
 
 # Limit to specific algorithms
 #methods = ["all"] # try all algos
 methods = ["logistic_regression",
            #"naive_bayes",
-           "svm_linear",
+           #"svm_linear",
            #"svm_sigmoid"
            ]
 #scales = [False, True]
-#methods = ["svm_linear", "svm_rbf", "svm_sigmoid"]
-#scales = [False]
 
-df_results = ml_loop(X_train, y_train, X_test, y_test, methods=methods, scales=[False, True])
+
+for train_size in train_sizes:
+    for vocab_size in vocab_sizes:
+        #train_size = 1000
+        #vocab_size = 500
+        
+        dataset_work = dataset.append(dataset2[:train_size], ignore_index=True)
+        corpus = build_corpus(dataset_work['Review'])
+        
+        # Creating the Bag of Words model for givn train+test sets
+        cv = CountVectorizer(max_features=vocab_size)
+        X = cv.fit_transform(corpus).toarray()
+        y = dataset_work.iloc[:, 1].values
+        
+        # set the matrix type to "sparse"
+        #X = sparse.csr_matrix(X)
+        
+        # Get back train and test sets form merged matrix
+        X_test = X[:TEST_SIZE]
+        X_train = X[TEST_SIZE:TEST_SIZE + train_size]
+        y_test = y[:TEST_SIZE]
+        y_train = y[TEST_SIZE:TEST_SIZE + train_size]
+        
+        df_results = ml_loop(X_train, y_train, X_test, y_test, methods=methods, scales=[False, True])
+
--- nlp_util.py
+++ nlp_util.py
@@ -9,6 +9,33 @@ Created on Fri May 11 01:10:55 2018
 import pandas as pd
 
 
+
+# dataset: test dataset
+# dataset2: train dataset
+#def build_sets(dataset_train, dataset_test, train_size, test_size, vocab_size):
+#    dataset_work = dataset.append(dataset2[:train_size], ignore_index=True)
+#    
+#    corpus = build_corpus(dataset_work['Review'][test_size + train_size])
+#    
+#    # Creating the Bag of Words model
+#    cv = CountVectorizer(max_features=vocab_size)
+#    X = cv.fit_transform(corpus).toarray()
+#    y = dataset_work.iloc[:, 1].values
+#    
+#    # set the matrix type to "sparse"
+#    #X = sparse.csr_matrix(X)
+#    
+#    # Get back train and test sets form merged matrix
+#    X_test = X[:test_size]
+#    X_train = X[test_size:test_size + train_size]
+#    y_test = y[:test_size]
+#    y_train = y[test_size:test_size + train_size]
+#    
+#    return X_train, X_test, y_test, y_train
+
+
+
+
 # Build a corpus from a text series
 def build_corpus(text_series):
     import re, nltk
@@ -34,7 +61,7 @@ def build_corpus(text_series):
     return corpus
 
 
-def ml_loop(X_train, y_train, X_test, y_test, methods=["all"], scales=[False, True], verbose=True, verboseSVM = False):
+def ml_loop(X_train, y_train, X_test, y_test, methods=["all"], scales=[False, True], verbose=True):
     """
     Parameters:
     X_train, y_train, X_test, y_test: training and test sets
@@ -46,7 +73,6 @@ def ml_loop(X_train, y_train, X_test, y_test, methods=["all"], scales=[False, Tr
     from sklearn.metrics import confusion_matrix
     from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score
     from time import process_time
-    from math import ceil
     from scipy import sparse
     from scipy.sparse.csr import csr_matrix
     #'scipy.sparse.csr.csr_matrix'
@@ -93,8 +119,6 @@ def ml_loop(X_train, y_train, X_test, y_test, methods=["all"], scales=[False, Tr
             if re_dense:
                 X_train = sparse.csr_matrix(X_train)
                 X_test = sparse.csr_matrix(X_test)
-            if verbose:
-                print("\nSCALING DONE")
         
         for method in methods:
             start_time = process_time()
@@ -129,15 +153,15 @@ def ml_loop(X_train, y_train, X_test, y_test, methods=["all"], scales=[False, Tr
             from sklearn.svm import SVC
             # SVM linear
             if method == "svm_linear":
-                classifier = SVC(kernel='linear', verbose=verboseSVM, random_state=0)
+                classifier = SVC(kernel='linear', random_state=0)
                 classifier.fit(X_train, y_train)
             # SVM RBF
             if method == "svm_rbf":
-                classifier = SVC(kernel='rbf', verbose=verboseSVM, random_state=0)
+                classifier = SVC(kernel='rbf', random_state=0)
                 classifier.fit(X_train, y_train)
             # SVM Sigmoid
             if method == "svm_sigmoid":
-                classifier = SVC(kernel='sigmoid', verbose=verboseSVM, random_state=0)
+                classifier = SVC(kernel='sigmoid', random_state=0)
                 classifier.fit(X_train, y_train)
             
             # Predict with current method
@@ -166,3 +190,19 @@ def ml_loop(X_train, y_train, X_test, y_test, methods=["all"], scales=[False, Tr
         print("\nEND ML_LOOP\n" + "#" * 20)
         print("TOTAL CPU TIME:", round(process_time() - start, 2))
     return df_results
+
+
+## TO CHECK
+## Compression
+#import seaborn as sns
+#
+#sparse_dataset = dataset
+##dense_size = np.array(dataset).nbytes/1e6
+##sparse_size = (sparse_dataset.data.nbytes + sparse_dataset.indptr.nbytes + sparse_dataset.indices.nbytes)/1e6
+#
+##sum(dataset.memory_usage())/ 1e6
+#
+#sns.barplot(['DENSE', 'SPARSE'], [dense_size, sparse_size])
+#plt.ylabel('MB')
+#plt.title('Compression')
+    
