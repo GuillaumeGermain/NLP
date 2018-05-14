02fb80d811c14d7fb2f1ce6150923b7ae4961b92
Guillaume Germain <g.germain@me.com>
2018-05-14 16:47:49 +0200

    Minor fixes
    Better readability

--- RNN/train_lstm.py
+++ RNN/train_lstm.py
@@ -1,31 +1,30 @@
 # coding: utf8
 import pandas as pd
 import numpy as np
-import gc
-import os
-import re
+import gc, os, re
 import tensorflow as tf
-import sys
-import math
-import datetime 
+import sys, math, datetime 
 from random import shuffle
 
+#MAIN_DIR = '/root/kaggle/restaurantAvis/cache/' #Alain
+MAIN_DIR = '' #Guillaume
 
-BEST_TRAIN_ACCURACY_MODEL = '/root/kaggle/restaurantAvis/cache/train_model.ckpt'
-BEST_DEV_ACCURACY_MODEL = '/root/kaggle/restaurantAvis/cache/dev_model.ckpt'
+
+BEST_TRAIN_ACCURACY_MODEL = MAIN_DIR + 'train_model.ckpt'
+BEST_DEV_ACCURACY_MODEL = MAIN_DIR + 'dev_model.ckpt'
 
 # Load Data
-MOTS_UTILES = '/root/kaggle/restaurantAvis/cache/mots_utiles.txt'
-MOTS_NECESSAIRES = '/root/kaggle/restaurantAvis/cache/mots_necessaires.txt'
-X_TRAIN_CSV = '/root/kaggle/restaurantAvis/cache/X_train.csv'
-Y_TRAIN_CSV = '/root/kaggle/restaurantAvis/cache/y_train.csv'
-X_DEV_CSV = '/root/kaggle/restaurantAvis/cache/X_dev.csv'
-Y_DEV_CSV = '/root/kaggle/restaurantAvis/cache/y_dev.csv'
-PRETRAINED_VOCAB_CSV = '/root/kaggle/restaurantAvis/cache/pretrained_vocab.csv'
-TO_TRAIN_VOCAB_CSV = '/root/kaggle/restaurantAvis/cache/to_train_vocab.csv'
+MOTS_UTILES = MAIN_DIR + 'mots_utiles.txt'
+MOTS_NECESSAIRES = MAIN_DIR + 'mots_necessaires.txt'
+X_TRAIN_CSV = MAIN_DIR + 'X_train.csv'
+Y_TRAIN_CSV = MAIN_DIR + 'y_train.csv'
+X_DEV_CSV = MAIN_DIR + 'X_dev.csv'
+Y_DEV_CSV = MAIN_DIR + 'y_dev.csv'
+PRETRAINED_VOCAB_CSV = MAIN_DIR + 'pretrained_vocab.csv'
+TO_TRAIN_VOCAB_CSV = MAIN_DIR + 'to_train_vocab.csv'
 
 EMB_DIM = 300 # 50,100, ou 300
-EMBS_CSV = '/root/kaggle/restaurantAvis/cache/pretrained_embs_' + str(EMB_DIM) + '.csv'
+EMBS_CSV = MAIN_DIR + 'pretrained_embs_' + str(EMB_DIM) + '.csv'
 
 pretrained_vocab = np.loadtxt(PRETRAINED_VOCAB_CSV, delimiter=';',dtype ='str')
 only_in_train = np.loadtxt(TO_TRAIN_VOCAB_CSV, delimiter=';',dtype ='str')
@@ -33,12 +32,15 @@ pretrained_embs = np.loadtxt(EMBS_CSV, delimiter=';')
 
 vocab = np.concatenate((pretrained_vocab, only_in_train))
 
+
+
+# Load the dataframes
 X_train = pd.read_csv(X_TRAIN_CSV)
 y_train = pd.read_csv(Y_TRAIN_CSV,names=['i','v'])
 X_dev = pd.read_csv(X_DEV_CSV)
 y_dev = pd.read_csv(Y_DEV_CSV,names=['i','v'])
 
-regularizer_scale= 0.05
+regularizer_scale = 0.05
 dropout_keep_prob = 0.7
 learning_rate = 0.001
 
@@ -47,27 +49,32 @@ REVIEW_LENGTH = 333
 
 tf.reset_default_graph()
 regularizer = tf.contrib.layers.l2_regularizer(scale=regularizer_scale)
+
 vocab_lookup = tf.contrib.lookup.index_table_from_tensor(
     mapping=tf.constant(vocab),
     default_value=len(vocab),
     dtype=tf.string)
+
 pretrained_embs_tf = tf.get_variable(
     name="embs_pretrained",
     initializer=tf.constant_initializer(pretrained_embs, dtype=tf.float32),
     shape=pretrained_embs.shape,
     trainable=False)
+
 train_embeddings = tf.get_variable(
     name="embs_only_in_train",
     shape=[len(only_in_train), EMB_DIM],
     initializer=tf.random_uniform_initializer(-0.04, 0.04),
     regularizer=regularizer,
     trainable=True)
+
 unk_embedding = tf.get_variable(
     name="unk_embedding",
     shape=[1, EMB_DIM],
     initializer=tf.random_uniform_initializer(-0.04, 0.04),
     regularizer=regularizer,
     trainable=True)
+
 embeddings = tf.concat([pretrained_embs_tf, train_embeddings, unk_embedding], axis=0)
 
 X_t_review = tf.placeholder(shape = [ None, REVIEW_LENGTH],dtype = tf.string, name = "X_t_review")
@@ -111,7 +118,9 @@ def dense_batch_relu(x, size, phase, scope):
                 scope='bn')
             return tf.nn.relu(h2, 'relu')
             
-def f2(net): return net
+def f2(net):
+    return net
+
 net = dense_batch_relu(lstm_review, 150, phase, 'layer1')
 net = tf.cond(phase, lambda: tf.nn.dropout(net, dropout_keep_prob), lambda: f2(net))
 net = dense_batch_relu(net, 100, phase, 'layer1b')
@@ -125,6 +134,7 @@ reg_term = tf.reduce_sum(reg_variables)
 cost = tf.reduce_mean(tf.square(preds - y_t))
 # optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 loss = cost + reg_term
+
 with tf.name_scope('accuracy'):
     accuracy = tf.reduce_mean(tf.abs(preds - y_t))
     # accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(y_t_2, 1), tf.argmax(logits, 1)),'float32'))
@@ -162,13 +172,17 @@ def prepare_mini_batch(X,y,k):
     
 init_op = tf.global_variables_initializer()
 init_tab = tf.tables_initializer()
+
+
 with tf.Session() as sess:
-    sess.run([init_op,init_tab])
+    sess.run([init_op, init_tab])
     if os.path.isfile(BEST_TRAIN_ACCURACY_MODEL + '.index'):
         print ("parameters loaded")
         saver.restore(sess, BEST_TRAIN_ACCURACY_MODEL)
+        
     best_train_accuracy = 0
     best_dev_accuracy = 0
+    
     for epoch in range(num_epochs):
         epoch_cost = 0
         train_accuracy = 0
